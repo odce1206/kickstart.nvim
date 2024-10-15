97bc4ea2110c5d3620e82a87165bb842be658a4a
soods <odce1206@gmail.com>
2024-10-15 03:55:01 -0600

    Final Modularization - Oc

--- lua/kickstart/plugins/utilities/neo-tree.lua
+++ lua/kickstart/plugins/utilities/neo-tree.lua
@@ -21,13 +21,13 @@ return {
         },
       },
     },
-    -- event_handlers = {
-    --   {
-    --     event = 'file_open_requested',
-    --     handler = function()
-    --       require('neo-tree.command').execute { action = 'close' }
-    --     end,
-    --   },
-    -- },
+    event_handlers = {
+      {
+        event = 'file_open_requested',
+        handler = function()
+          require('neo-tree.command').execute { action = 'close' }
+        end,
+      },
+    },
   },
 }
